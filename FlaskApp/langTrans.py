c16486bc5a218d35b2776406c65b425d18946300
Neha-Patil-12 <106377584+Neha-Patil-12@users.noreply.github.com>
2023-05-03 01:18:54 +0530

    Delete langTrans.py

--- FlaskApp/langTrans.py
+++
@@ -1,7 +0,0 @@
-from googletrans import Translator
-
-translater=Translator()
-
-sentence="Pune: H3N2 Virus Claims Second Life In Pimpri-Chinchwad As COVID-19 Cases Rise Again"
-out=translater.translate(sentence, dest="hi")
-print(out.text)
